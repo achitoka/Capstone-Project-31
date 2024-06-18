75477d820ce366b904439ca0c10af70458afcc39
Mishellrd9 <151493734+Mishellrd9@users.noreply.github.com>
2024-06-18 12:24:30 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,29 @@
 import bcrypt
 import sqlite3
-from db import get_db_connection
+
+db_config = 'dbtomat.db'
+# Fungsi untuk membuat koneksi ke database SQLite
+def get_db_connection():
+    try:
+        conn = sqlite3.connect(db_config)
+        return conn
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+        return None
+
+def create_user_table():
+    conn = get_db_connection()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
+        conn.close()
 
 def add_user(username, password):
     try:
