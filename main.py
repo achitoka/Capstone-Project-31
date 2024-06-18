09b8436da7952eb2cde1b69850f0cec0851dfc81
Mishellrd9 <151493734+Mishellrd9@users.noreply.github.com>
2024-06-18 12:24:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from login import login_page
 from signup import signup_page
 from index import camera_scan_page, gallery_and_details_page, homepage
-from db import get_db_connection
+from auth import get_db_connection
 
 # def set_custom_css():
 #     st.markdown(
